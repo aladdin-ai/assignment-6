73c6da2916068793472a2e7de5355f2c94233856
aladdine adel <alaaelden997@gmail.com>
2021-03-20 09:22:13 +0200

    uploading thanos project & modules file & errors file

---
+++ my_packages/Operations/Math.py
@@ -0,0 +1,6 @@
+def get_average(l):
+	avg = 0
+	sum = 0
+	for i in l:
+		sum += i
+	return [sum/len(l)]
---
+++ my_packages/Transformers/StringTransformer.py
@@ -0,0 +1,4 @@
+def  string_reversing(word):
+	return word[::-1]
+def string_capitalize(word):
+	return word.capitalize()
